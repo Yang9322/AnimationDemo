098e211599a50eeb746d22a0292aa8bd69dd7e33
Yang9322 <576421791@qq.com>
2015-12-18 15:39:54 +0800

    Initial Commit

---
+++ AnimationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationDemo
+//
+//  Created by He yang on 15/12/18.
+//  Copyright © 2015年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
